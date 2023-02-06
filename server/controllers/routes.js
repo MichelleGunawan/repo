b395ce9c42b6c7cdf911d3efe137e675d08a3da4
ingriddleee <47064645+ingriddleee@users.noreply.github.com>
2023-02-05 22:05:07 -0800

    implemented basic user register login logout authentication (#2)

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -3,3 +3,9 @@ exports.getTest = async(req, res) => {
         message: "Test API is working!",
     })
 }
+
+exports.register = async(req, res) => {
+    res.status(200).json({
+        message: "Register API is working!",
+    })
+}
