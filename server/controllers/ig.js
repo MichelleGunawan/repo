b4c6c5811ed7767ca51877f0903293117fd5efa5
MichelleGunawan <michellegunawan9999@outlook.com>
2023-02-23 17:12:39 -0800

    return just saved post codes

--- server/controllers/ig.js
+++ server/controllers/ig.js
@@ -10,12 +10,17 @@ async function login(ig_username, ig_password) {
 
 async function igSavedPosts(req, res) {
   try {
-    await login(req.body.ig_username, req.body.ig_password);
+    console.log(req.body);
+    const { ig_username, ig_password } = req.body;
+    await login(ig_username, ig_password);
 
     const savedFeed = ig.feed.saved();
     const savedItems = await savedFeed.items();
+    
+    const codes = (savedItems).map(obj => obj.code);
+    console.log(codes);
 
-    res.status(200).json(savedItems);
+    res.status(200).json(codes);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
