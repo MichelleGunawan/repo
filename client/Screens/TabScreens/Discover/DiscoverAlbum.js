12b38befa545567db31c3da05eddb82c3fe8a980
jesse samuel widjaja <jesse@jesses-MacBook-Pro.local>
2023-03-02 20:04:54 -0800

    discover page grid

--- client/Screens/TabScreens/Discover/DiscoverAlbum.js
+++ client/Screens/TabScreens/Discover/DiscoverAlbum.js
@@ -1,13 +1,97 @@
-import React, { useState } from "react";
-import { View, FlatList, Button, Image, Text } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Grid from "../../../components/Grid/Grid";
-import SearchBar from "../../../components/SearchBar/SearchBar";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+const data = [
+  { key: "A" },
+  { key: "B" },
+  { key: "C" },
+  { key: "D" },
+  { key: "E" },
+  { key: "F" },
+  { key: "G" },
+  { key: "H" },
+  { key: "I" },
+  { key: "J" },
+  { key: "K" },
+];
+
+const numColumns = 2;
+
+const formatData = (data, numColumns) => {
+  const numberOfFullRows = Math.floor(data.length / numColumns);
+
+  let numberOfElementsLastRow = data.length - numberOfFullRows * numColumns;
+  while (
+    numberOfElementsLastRow !== numColumns &&
+    numberOfElementsLastRow !== 0
+  ) {
+    data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+    numberOfElementsLastRow++;
+  }
+
+  return data;
+};
 
 export default function DiscoverAlbum() {
-	return (
-		<View>
-			<Grid />
-		</View>
-	);
+  const navigation = useNavigation();
+
+  const renderItem = ({ item }) => {
+    const handleButtonPress = () => {
+      navigation.navigate("ProfilePage");
+    };
+
+    if (item.empty === true) {
+      return <View style={[styles.item, styles.itemInvisible]} />;
+    }
+    return (
+      <TouchableOpacity onPress={handleButtonPress} style={styles.item}>
+        <Text style={styles.itemText}>{item.key}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList
+      data={formatData(data, numColumns)}
+      style={styles.container}
+      renderItem={renderItem}
+      numColumns={numColumns}
+      showsVerticalScrollIndicator={false}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    marginBottom: 30,
+  },
+
+  item: {
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    margin: 10,
+    height: Dimensions.get("window").width / numColumns - 10, // approximate a square
+    borderRadius: "5px",
+  },
+
+  itemInvisible: {
+    backgroundColor: "transparent",
+  },
+
+  itemText: {
+    color: "#fff",
+  },
+});
