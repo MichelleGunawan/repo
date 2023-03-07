1be16cd358eb7cf3c240371efa7162a6bfb3f401
jesse samuel widjaja <jesse@jesses-MacBook-Pro.local>
2023-03-06 22:33:16 -0800

    masonry layout

--- client/Screens/TabScreens/ProfileScreens/ProfilePage.js
+++ client/Screens/TabScreens/ProfileScreens/ProfilePage.js
@@ -1,33 +1,55 @@
-import { View, FlatList, Image, Text } from "react-native";
-
-const mockData = [
-  { id: "1", text: "React", img: "https://picsum.photos/200/300?random=2" },
-  { id: "2", text: "is", img: "https://picsum.photos/200/300?random=2" },
-  { id: "3", text: "Awesome!", img: "https://picsum.photos/200/300?random=2" },
-];
+import { View, FlatList, Image, Text, StyleSheet } from "react-native";
+import MasonryList from "@react-native-seoul/masonry-list";
 
 export default function ProfilePage() {
-  return (
+  const DATA = [
+    { id: "1", height: 70 },
+    { id: "2", height: 130 },
+    { id: "3", height: 90 },
+    { id: "4", height: 86 },
+    { id: "5", height: 110 },
+    { id: "6", height: 40 },
+    { id: "7", height: 70 },
+    { id: "8", height: 130 },
+    { id: "9", height: 90 },
+    { id: "10", height: 86 },
+    { id: "11", height: 110 },
+    { id: "12", height: 40 },
+  ];
+
+  const renderItem = ({ item }) => (
     <View
-      style={{
-        margin: 10,
-      }}>
-      <FlatList
-        data={mockData}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={{ margin: 10, flexDirection: "row" }}>
-            <Image
-              style={{ width: 50, height: 50, borderRadius: 25 }}
-              source={{ uri: "https://picsum.photos/200/300?random=2" }}
-            />
-            <View>
-              <Text style={{ fontSize: 22 }}>{item.text}</Text>
-              <Text style={{ fontSize: 22 }}>{item.text}</Text>
-            </View>
-          </View>
-        )}
-      />
-    </View>
+      style={[styles.item, { height: item.height, backgroundColor: "grey" }]}
+    />
+  );
+
+  return (
+    <MasonryList
+      data={DATA}
+      style={styles.container}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+      columnWrapperStyle={styles.columnWrapper}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 5,
+    marginBottom: 30,
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+  },
+  item: {
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    margin: 10,
+    borderRadius: "5px",
+  },
+});
